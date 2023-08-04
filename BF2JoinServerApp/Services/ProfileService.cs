6b12e193f402ff072337f1103cd1b913a8b2dd33
WSSimcox <WilliamSSimcox@gmail.com>
2023-08-04 12:40:10 -0700

    .gitignore created
    Updated test profiles

--- BF2JoinServerApp/Services/ProfileService.cs
+++ BF2JoinServerApp/Services/ProfileService.cs
@@ -42,14 +42,15 @@ namespace BF2JoinServerApp.Services
         {
             return new List<Profile>
             {
-                new Profile("JoeBiden", "JoeBiden", 120, 0),
-                new Profile("BiggoNiggo", "JoeBiden", 120, 0),
-                new Profile("WonkasFirstChoice", "JoeBiden", 120, 0),
-                new Profile("ButtholeSurfer", "JoeBiden", 120, 0),
-                new Profile("Shed5", "JoeBiden", 120, 0),
-                new Profile("Shed6", "JoeBiden", 120, 0),
-                new Profile("Shed7", "JoeBiden", 120, 0),
-                new Profile("Shed8", "JoeBiden", 120, 0),
+            new Profile("Shed1", "Shed1", 120, 69),
+            new Profile("Shed2", "Shed2", 120, 420),
+            new Profile("Shed3", "Shed3", 120, 1),
+            new Profile("Shed4", "Shed4", 120, 100),
+            new Profile("Shed5", "Shed5", 120, 1000),
+            new Profile("Shed6", "Shed6", 120, 1337),
+            new Profile("Shed7", "Shed7", 120, 0),
+            new Profile("Shed8", "Shed8", 120, 64),
+            new Profile("Shed9", "Shed9", 120, 128)
             };
         }
     }

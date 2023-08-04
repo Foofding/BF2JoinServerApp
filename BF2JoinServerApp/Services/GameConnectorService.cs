26caa405d90bdead6f59c2e09fa68b0a7ce9b9ec
Toby Simcox <tobyrs@hotmail.com>
2023-08-04 04:06:07 -0700

    join button functionality test 1. IPgathering and joining

--- BF2JoinServerApp/Services/GameConnectorService.cs
+++ BF2JoinServerApp/Services/GameConnectorService.cs
@@ -3,15 +3,41 @@ using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 
+
 namespace BF2JoinServerApp
 {
     class GameConnectorService
     {
-        public void HostGame()
-        {
-            int port = 12345;
-            TcpListener server = new TcpListener(IPAddress.Any, port);
+        private int _port = 12345;
+        public string HostIP { get; set; }
+        public void HostGame()        {
+            
+            TcpListener server = new TcpListener(IPAddress.Any, _port);
             server.Start();
+        }
+
+        public string JoinGame()
+        {
+            for (int i = 1; i <= 255; i++) // Scanning IPs from 192.168.1.1 to 192.168.1.255
+            {
+                string ipAddress = $"192.168.1.{i}";
+
+                try
+                {
+                    TcpClient client = new TcpClient();
+                    client.Connect(ipAddress, _port);
+                    if (client.Connected)
+                    {
+                        return ipAddress;
+                    }                    
+                }
+                catch (SocketException)
+                {
+                    // The port is not open on this host
+                }
+            }
+            return null;
+
         }
 
         //"+modPath mods/bf2all64"

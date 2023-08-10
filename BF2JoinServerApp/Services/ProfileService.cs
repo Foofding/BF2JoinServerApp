da22337a4256b94eeebcf00d90bec72436e9a18e
William Simcox <williamssimcox@gmail.com>
2023-08-09 20:22:43 -0700

    Removed test function

--- BF2JoinServerApp/Services/ProfileService.cs
+++ BF2JoinServerApp/Services/ProfileService.cs
@@ -87,25 +87,5 @@ namespace BF2JoinServerApp.Services
         {
             _profileRepository.CopyProfile(profileFolderName);
         }
-
-        /// <summary>
-        /// Method to get a list of test profiles (for testing purposes)
-        /// </summary>
-        /// <returns>A list of test Profile objects</returns>
-        //public List<Profile> GetTestProfiles()
-        //{
-        //    return new List<Profile>
-        //    {
-        //        new Profile("Shed1", "Shed1", 120, 69),
-        //        new Profile("Shed2", "Shed2", 120, 420),
-        //        new Profile("Shed3", "Shed3", 120, 1),
-        //        new Profile("Shed4", "Shed4", 120, 100),
-        //        new Profile("Shed5", "Shed5", 120, 1000),
-        //        new Profile("Shed6", "Shed6", 120, 1337),
-        //        new Profile("Shed7", "Shed7", 120, 0),
-        //        new Profile("Shed8", "Shed8", 120, 64),
-        //        new Profile("Shed9", "Shed9", 120, 128)
-        //    };
-        //}
     }
 }

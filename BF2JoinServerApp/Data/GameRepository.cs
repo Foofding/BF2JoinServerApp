ada10fa558f3163ba5546ccb7e274d06df8b953d
William Simcox <williamssimcox@gmail.com>
2023-08-09 20:31:50 -0700

    GameService created

--- BF2JoinServerApp/Data/GameRepository.cs
+++ BF2JoinServerApp/Data/GameRepository.cs
@@ -3,26 +3,44 @@ using System.IO;
 
 namespace BF2JoinServerApp.Data
 {
+    /// <summary>
+    /// Represents a repository for accessing BF2-related information
+    /// </summary>
     public class GameRepository
     {
         public Game _game { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the GameRepository class
+        /// </summary>
         public GameRepository()
         {
             _game = new Game();
             CheckInstallation();
         }
 
+        /// <summary>
+        /// Gets the directory path of the game
+        /// </summary>
+        /// <returns>The directory path of the game</returns>
         public string GetDirectoryPath()
         {
             return _game.DirectoryPath;
         }
 
+        /// <summary>
+        /// Gets the executable path of the game
+        /// </summary>
+        /// <returns>The executable path of the game</returns>
         public string GetExecutablePath()
         {
             return _game.ExecutablePath;
         }
 
+        /// <summary>
+        /// Checks if the game is installed
+        /// </summary>
+        /// <returns>True if the game is installed, false otherwise</returns>
         public bool CheckInstallation()
         {
             if (Directory.Exists(_game.DirectoryPath))
---
+++ BF2JoinServerApp/Services/GameService.cs
@@ -0,0 +1,48 @@
+﻿using BF2JoinServerApp.Data;
+using System;
+
+namespace BF2JoinServerApp.Services
+{
+    /// <summary>
+    /// Provides services for interacting with the BF2-related information
+    /// </summary>
+    public class GameService
+    {
+        private readonly GameRepository _gameRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the GameService class
+        /// </summary>
+        public GameService()
+        {
+            _gameRepository = new GameRepository();
+        }
+
+        /// <summary>
+        /// Gets the directory path of the game
+        /// </summary>
+        /// <returns>The directory path of the game</returns>
+        public string GetDirectoryPath()
+        {
+            return _gameRepository.GetDirectoryPath();
+        }
+
+        /// <summary>
+        /// Gets the executable path of the game
+        /// </summary>
+        /// <returns>The executable path of the game</returns>
+        public string GetExecutablePath()
+        {
+            return _gameRepository.GetExecutablePath();
+        }
+
+        /// <summary>
+        /// Checks if the game is installed
+        /// </summary>
+        /// <returns>True if the game is installed, false otherwise</returns>
+        public bool CheckInstallation()
+        {
+            return _gameRepository.CheckInstallation();
+        }
+    }
+}
